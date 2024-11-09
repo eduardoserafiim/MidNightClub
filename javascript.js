71d7e72caba2e222e4ccc7ff0473a1e48999ba2e
Eduardo Serafim <eduardoserafiim05@gmail.com>
2024-11-09 16:21:29 -0400

    novo
    terminei de dizer o que cada um fez, e também falta apenas fazer quando o usuario clicar na foto, o blgh descer até embaixo aonde ta a descrição

--- javascript.js
+++ javascript.js
@@ -5,26 +5,32 @@ const fotoqueiroz = document.getElementById('imagem4');
 const fotofernandes = document.getElementById('imagem5');
 const body = document.getElementById('bodyfoda');
 
-
-
 fotosales.addEventListener('click', function(){
     console.log('runing...')
-    body.innerHTML += "<div class='card1'><h1>João Sales</h1><p>Mais Conhecido com João 'GOAT' Sales Cola Pinto, foi reconhecido pelo seu enorme trabalho de desenvolvimento e atividade na empresa NightClub.</p><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/Moe-L3ster' target='_blank'>GitHub</a></div></div>"
+    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>João Sales</h1><p>João 'GOAT' Sales, jogava profissionalmente LOL, mas felizmente, desistiu da carreira e veio estudar no SENAC no curso de Analise e Desenvolvimento de Sistemas para ser alguem na vida, obrigado. Fez a parte dos cards de ver a inscrição, foi por ai.</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/thenewgrau' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/eduardo-serafim-821649305/' target='_blank'>Linkedin</a></div></div></div>"
+
 });
 
 fotoheitor.addEventListener('click', function(){
     console.log('runing2...')
+    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>João Heitor</h1><p>Um louco e sonhador trabalhador, sinceramente, tenho orgulho desse rapaz, fez a parte de inscrição maravilhosamente, não podia pedir mais desse sujeito de boa vontade. Deve ter 17 anos, não sei, gosta de Drake e tem 1,72 de altura, tamanho do pé = 41, 42 (sinsitro).</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/thenewgrau' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/eduardo-serafim-821649305/' target='_blank'>Linkedin</a></div></div></div>"
+
 });
 
 fotoserafim.addEventListener('click', function(){
     console.log('runing3...')
-    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>Eduardo Serafim</h1><p>Conhecido como Eduardo 'Walter White' Serafim, trabalhou honestamente e esforçadamente para a conclusão desse trabalho exaustivo, merece o pagamento do seu 13° e também pensa que quem leu isso até o final é gayr. OBS:(Por favor Julia me desculpa)</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/thenewgrau' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/eduardo-serafim-821649305/' target='_blank'>Linkedin</a></div></div></div>"
+    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>Eduardo Serafim</h1><p>Conhecido como Eduardo 'Walter White' Serafim, trabalhou honestamente na parte de about us (para os não gringos sobre nós), e também para a conclusão desse trabalho exaustivo, merece o pagamento do seu 13° e também pensa que quem leu isso até o final é gay.</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/thenewgrau' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/eduardo-serafim-821649305/' target='_blank'>Linkedin</a></div></div></div>"
+
 });
 
 fotoqueiroz.addEventListener('click', function(){
     console.log('runing4...')
+    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>Henrico Queiroz</h1><p>Altamente procurado na sociedade urugaiana para renovação da sua matricula. Forte personalidade e braços carnudos, está sendo procurado no Chile por hackear pessoas. Um ótimo programador pois fez a parte quase inteira do  main, num sei, esqueci o nome, acho que é main, alguma coisa assim sla.</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/HenricQ' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/henrico-queiroz-725007325/' target='_blank'>Linkedin</a></div></div></div>"
+
 });
 
 fotofernandes.addEventListener('click', function(){
     console.log('runing5...')
+    body.innerHTML += "<div id='numseimaiorqueooceano'><h1>Robert Fernandes</h1><p>Fez o rodapé do site, tem sinistro no nome e também está individado pois comprou um processador (Ryzen 5 4600g). O Sales também tem, parabéns Sales. Eu amo muito o Sales, acho que quero casar com ele... Robert gosta de fazer pequiniques e também ficar em casa pelado. (ele mesmo que disse)</p><div id='oceano'><div id='agua'><img id='github' src='github.png' alt='github'><a href='https://github.com/thenewgrau' target='_blank'>GitHub</a></div><div id='agua'><img id='linkedin' src='linkedin.png' alt='linkedin'><a href='https://www.linkedin.com/in/eduardo-serafim-821649305/' target='_blank'>Linkedin</a></div></div></div>"
+
 });
